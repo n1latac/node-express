f159bed9bb5e214b6eb79e877ebfe81f73f63a6a
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-02-23 22:39:25 +0200

    html page after request on server

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const http = require('node:http');
+const fs = require('node:fs/promises');
+
 const PORT = 3000;
-const requestListener = (req, res) => {
-    console.log(req);
+
+const requestListener = async (req, res) => {
+  const { url } = req;
+
+  if(url === '/index.html/'){
+    try{
+      const data = await fs.readFile('./views/index.html', 'utf-8');
+      res.end(data);
+    } catch(error){
+      res.statusCode = 404;
+      res.end(error)
+    }
+  } else if(url === '/index.html/style.css'){
+    try{
+      const data = await fs.readFile('./views/style.css', 'utf-8');
+      res.end(data);
+    } catch(error){
+      res.statusCode = 404;
+      res.end(error);
+    }
+  } else{
+    res.statusCode = 404;
+    res.end();
+  }
+
 }
 
 const server = http.createServer(requestListener);
+
 server.listen(PORT);
