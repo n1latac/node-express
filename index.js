7caec7e4be51dab2727120d11f373f22cf34f23b
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-02-25 16:47:31 +0200

    express start

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-const http = require('node:http');
-const fs = require('node:fs/promises');
-
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-const requestListener = async (req, res) => {
-  const { url } = req;
 
-  if(url === '/index.html/'){
-    try{
-      const data = await fs.readFile('./views/index.html', 'utf-8');
-      res.end(data);
-    } catch(error){
-      res.statusCode = 404;
-      res.end(error)
-    }
-  } else if(url === '/index.html/style.css'){
-    try{
-      const data = await fs.readFile('./views/style.css', 'utf-8');
-      res.end(data);
-    } catch(error){
-      res.statusCode = 404;
-      res.end(error);
-    }
-  } else{
-    res.statusCode = 404;
-    res.end();
-  }
+app.get('/', (req,res) => {
+  res.send('Hello world');
+})
 
-}
-
-const server = http.createServer(requestListener);
-
-server.listen(PORT);
+app.listen(PORT, ()=>{
+  console.log(`Server started on port ${PORT}`)
+})
